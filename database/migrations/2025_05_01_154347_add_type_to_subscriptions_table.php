43af1ec0471b16f36d2ffdd29aa27d0493b990e4
SalihBorucu <salih_borucu@hotmail.com>
2025-05-01 16:53:37 +0100

    fix for subscriptions table

---
+++ database/migrations/2025_05_01_154347_add_type_to_subscriptions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->string('type')->after('stripe_status')->default('band');
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropColumn('type');
+            $table->string('name');
+        });
+    }
+};
